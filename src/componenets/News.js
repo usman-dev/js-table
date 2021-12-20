002b8cb110a4a8a63c6776cb5579086da9c294e6
usman-dev <usman.axhraf@gmail.com>
2021-12-20 16:35:56 +0500

    Fixed url added on missing images

--- src/componenets/News.js
+++ src/componenets/News.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-articles= []
 
     constructor(){
         super()
         this.state = {
-            articles: this.articles,
+            articles: [],
             loading: false
         }
     }
 
     async componentDidMount(){
-        console.log("did mount")
         let url = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9ba4796f37b1465bb15222ed537beeb3"
         let data = await fetch(url)
         let parsedData = await data.json()
@@ -21,7 +19,7 @@ articles= []
         this.setState({articles: parsedData.articles})
 
     }
-
+//
     render() {
         return (
             <div className="container my-4">
@@ -30,7 +28,7 @@ articles= []
                     {this.state.articles.map((element)=>{
                         return <div className="col-md-4" key={element.url}>
                         <NewsItem title={element.title} description={element.description}
-                        imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        imageUrl={element.urlToImage? element.urlToImage:"https://cdn.wccftech.com/wp-content/uploads/2021/12/xcyDFMVt7v9FzyMgCUa8e4-1024-80.jpg.webp"} newsUrl={element.url}/>
                     </div>
                     })}
                     
@@ -39,4 +37,3 @@ articles= []
         )
     }
 }
-//9ba4796f37b1465bb15222ed537beeb3
