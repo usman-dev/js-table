8b37c87dd852efac53a9fd72ae82d3bae5e7eaf3
usman-dev <usman.axhraf@gmail.com>
2021-12-21 19:49:02 +0500

    To navigate articles, Next and Previous buttons were added with disabled fuction

--- src/componenets/News.js
+++ src/componenets/News.js
@@ -7,19 +7,47 @@ export default class News extends Component {
         super()
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page:1
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9ba4796f37b1465bb15222ed537beeb3"
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9ba4796f37b1465bb15222ed537beeb3&page=1&pageSize=20`;
         let data = await fetch(url)
         let parsedData = await data.json()
         console.log(parsedData)
-        this.setState({articles: parsedData.articles})
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
 
     }
-//
+
+    handleNext = async()=>{
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9ba4796f37b1465bb15222ed537beeb3&page=${this.state.page + 1}&pageSize=20`; 
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        })
+    }
+    }
+
+    handlePrev = async()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=9ba4796f37b1465bb15222ed537beeb3&page=${this.state.page - 1}&pageSize=20`; 
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
     render() {
         return (
             <div className="container my-4">
@@ -30,9 +58,12 @@ export default class News extends Component {
                         <NewsItem title={element.title} description={element.description}
                         imageUrl={element.urlToImage? element.urlToImage:"https://cdn.wccftech.com/wp-content/uploads/2021/12/xcyDFMVt7v9FzyMgCUa8e4-1024-80.jpg.webp"} newsUrl={element.url}/>
                     </div>
-                    })}
-                    
-                </div>           
+                    })}   
+                </div> 
+                <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-primary btn-lg" onClick={this.handlePrev}>&larr; Previous</button>
+                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-primary btn-lg" onClick={this.handleNext}>Next &rarr;</button>
+                </div>          
             </div>
         )
     }
